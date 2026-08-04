3d51b73ee326bba55596acc3219e892bc3cc2111
Muhtasham Nawr al-Mahmud <muhtaseem2005@gmail.com>
2026-08-04 13:05:08 +0600

    Test AppImage action build

--- LumaGtk/build-aux/appimage/make-appimage.sh
+++ LumaGtk/build-aux/appimage/make-appimage.sh
@@ -7,7 +7,7 @@ VERSION=${LUMA_VERSION:-1.0.0}
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm libgee
+pacman -Syu --noconfirm libgee libadwaita webkitgtk-6.0 libepoxy libzip libnice
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
